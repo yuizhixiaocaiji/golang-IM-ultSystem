47d1ccbeb6f15b9a080337218266e05d211d45ff
yuizhixiaocaiji <1390683299@qq.com>
2023-05-08 20:40:58 +0800

    新增登录结构体

---
+++ models/UserBasic.go
@@ -0,0 +1,23 @@
+package models
+
+import "github.com/jinzhu/gorm"
+
+type UserBasic struct {
+	gorm.Model
+	Name          string
+	PassWord      string
+	Phone         string
+	Email         string
+	Identity      string
+	ClientIp      string
+	ClientPort    string
+	LoginTime     uint64
+	HeartbeatTime uint64
+	LogOutTime    uint64
+	IsLogout      bool
+	DeviceInfo    string
+}
+
+func (table *UserBasic) TableName() string {
+	return "user_basic"
+}
